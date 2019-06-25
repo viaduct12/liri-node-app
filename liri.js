22929c3a220a0f391b6558c1c1ca9c11619eb51c
Lawrence Fiesta <lfiesta86@gmail.com>
2019-06-25 16:53:16 -0700

    half completed song bot

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-// var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 
 //original input of user
@@ -17,7 +18,13 @@ userInputCopy.splice(0,1);
 var searchText = userInputCopy;
 
 var concertObject = {};
-var songObject = {};
+var songObject = {
+  "Artist(s)": "",
+  "Song Name": "",
+  "Preview Link": "",
+  Album: "",
+};
+
 var movieObject = {
   Title: "",
   Year: "",
@@ -39,6 +46,7 @@ switch (operation) {
 
   case "spotify-this-song":
     console.log("spotify this song", operation);
+    console.log(searchText);
     song(searchText);
     break;
 
@@ -59,7 +67,42 @@ function concert(text){
 }
 
 function song(text) {
+  var songName = "The Sign"
+  if (text.length !== 0) {
+    songName = text.join(" ");
+  }
 
+  console.log(songName);
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else {
+      // console.log(data.tracks);
+      // console.log(data.tracks.items[0].album.name);
+      // var artistName = [];
+      for(var i = 0; i < data.tracks.items.length; i++){
+        // console.log("crap " + i);
+        var itemsArray = data.tracks.items[i];
+        songObject = {
+          "Song Name": data.tracks.items[i].name,
+          "Preview Link": data.tracks.items[i].preview_url,
+          Album: data.tracks.items[i].album.name
+        }
+        var tempArtist = "";
+
+        for(var j= 0; j < itemsArray.artists.length; j++){
+          // artistName.push(itemsArray.artists[j].name);
+          tempArtist += itemsArray.artists[j].name + ", ";
+          songObject = {
+            "Artist(s)" : tempArtist
+          }
+        };
+        console.log(songObject);
+        // printInfo(songObject);
+      }
+      // console.log(artistName);
+    }
+  })
 }
 
 function movie(text) {
@@ -70,7 +113,7 @@ function movie(text) {
   
   axios({
     method: "GET",
-    url: "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=BLANK"
+    url: "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + process.env.OMDB_ID
   }).then(movie => {
     var response = movie.data;
     // console.log(response);
